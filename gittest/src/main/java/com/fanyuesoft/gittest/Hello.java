1ef738cda4fe1d71c479ba3621d030d73a14ffd2
545877604 <545877604@qq.com>
2019-01-21 15:23:37 +0800

    提交branche1

--- gittest/src/main/java/com/fanyuesoft/gittest/Hello.java
+++ gittest/src/main/java/com/fanyuesoft/gittest/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+		System.out.println("11111111");
+		System.out.println("22222222");
+	}  
 
 }
