1de8e3cd6a5693702ef8c997647701e40a1b1d05
545877604 <545877604@qq.com>
2019-01-21 16:58:05 +0800

    修改

--- gittest/src/main/java/com/fanyuesoft/gittest/Hello.java
+++ gittest/src/main/java/com/fanyuesoft/gittest/Hello.java
@@ -4,6 +4,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("11111111");
+		
+		System.out.println("stashes");
 	
 	
 	}  
